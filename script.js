12a56f2a845fc49ed23faf7f3700e496bf8c822b
acemavrick <116685696+acemavrick@users.noreply.github.com>
2023-05-17 18:20:12 +0000

    Put JS to measure dimensions.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-null
+// change the contents of "dimensions" to width and height of viewport in pixels every time it changes
+var pastDimensions = [window.innerWidth, window.innerHeight];
+while (true) {
+  var dimensions = [window.innerWidth, window.innerHeight];
+  if (dimensions != pastDimensions) {
+    pastDimensions = dimensions;
+    document.getElementById("dimensions").innerHTML = dimensions[0] + "x" + dimensions[1] + "px";
+  }
+}
